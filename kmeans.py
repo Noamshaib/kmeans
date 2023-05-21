9589868b50eb09617e5cf37e9553c119a848be7d
Noamshaib <noamshaib@mail.tau.ac.il>
2023-05-21 15:57:00 +0300

    "final"

--- kmeans.py
+++ kmeans.py
@@ -6,13 +6,13 @@ import sys
 
 def k_means(k, iter, input_data):
     # providing K "best" centroids of N points in R^d, allowing error of eps=0.001
-    try:
+
     # assert iter requirements
     if iter <= 1 or iter >= 1000:
-        raise Exception("Invalid maximum iteration!")
+        return "Invalid maximum iteration!"
     # assert K is natural > 1 requirement
     if k <= 1:
-        raise Exception("Invalid number of clusters!")
+        return "Invalid number of clusters!"
 
     # store the vectors from the txt file in a list, each vector store as a list of d point
     points = []
@@ -28,7 +28,7 @@ def k_means(k, iter, input_data):
 
     # assert K < N requirement
     if k >= N:
-        raise Exception("Invalid number of clusters!")
+        return "Invalid number of clusters!"
 
     # k-mean algorithm
     centroids = points[:k]  # initialize centroids as first k data_points
@@ -36,8 +36,8 @@ def k_means(k, iter, input_data):
         partition = []  # saves j for each point (from below) by order of points
         for pointt in points:
             dis_from_cents = [euc_dis(pointt, centroid) for centroid in centroids]  # list of distances from centroids
-            j = dis_from_cents.index(min(dis_from_cents))   # the index of the closest centroid
-            partition.append(j) # store in partition
+            j = dis_from_cents.index(min(dis_from_cents))  # the index of the closest centroid
+            partition.append(j)  # store in partition
 
         if t == iter - 1:  # if its the last iteration we don't want to update the centroids
             centroids = [[float(f"{num:.4f}") for num in cenntroidd] for cenntroidd in centroids]
@@ -46,18 +46,18 @@ def k_means(k, iter, input_data):
         # initialize the new centroids
         new_centroids = []
         for j in range(k):
-            points_sum = [0]*d  # sum of points that belong to cluster j
+            points_sum = [0] * d  # sum of points that belong to cluster j
             cnt_point = 0  # num of points in cluster j
             for i in range(N):
                 if partition[i] == j:
                     points_sum = vec_sum(points_sum, points[i])
                     cnt_point += 1
-            new_centroid = [(1/cnt_point)*point for point in points_sum]
+            new_centroid = [(1 / cnt_point) * point for point in points_sum]
             new_centroids.append(new_centroid)
 
         # check if all the distance between new centroid and old one is smaller than eps
         eps = 0.001
-        max_dist = 0    # store the maximum distance old centroid to new one
+        max_dist = 0  # store the maximum distance old centroid to new one
         for i in range(k):
             centroids_dis = euc_dis(centroids[i], new_centroids[i])
             max_dist = max(max_dist, centroids_dis)
@@ -81,8 +81,8 @@ def euc_dis(v1, v2):
 
 def vec_sum(v1, v2):
     # providing the sum of 2 vectors
-    d=len(v1)
-    return [v1[i]+v2[i] for i in range(d)]
+    d = len(v1)
+    return [v1[i] + v2[i] for i in range(d)]
 
 
 # Call the k_mean function with the provided command-line arguments
@@ -93,8 +93,7 @@ if len(sys.argv) == 3:
 else:
     iter = int(sys.argv[2])
     txt_file = sys.argv[3]
-
-print(k_means(k, iter, txt_file))
-
-
-
+try:
+    print(k_means(k, iter, txt_file))
+except Exception as e:
+    print("An Error Has Occurred")
